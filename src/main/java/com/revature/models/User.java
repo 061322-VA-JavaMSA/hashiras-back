ca0f6d32454f6d8c3359f482558d2a487cc405cd
lanem123 <lanedmcspadden@gmail.com>
2022-07-20 14:39:31 -0500

    Added Annotations for the User class

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -2,13 +2,35 @@ package com.revature.models;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
+	
+	//Variable Fields
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	@Column(nullable=false, unique = true)
 	private String username;
+	@Column(nullable=false)
 	private String password;
+	@Column(nullable=false)
 	private String fname;
+	@Column(nullable=false)
 	private String lname;
+	@Column(nullable=false)
 	private String email;
+	@Column(nullable = false)
+	private int status;
+	
 	
 	//Getters and Setters
 	
@@ -48,14 +70,27 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	//To String
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", fname=" + fname + ", lname="
+				+ lname + ", email=" + email + ", status=" + status + "]";
+	}
 	
 	//Hash and Equals
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(email, fname, id, lname, password, username);
+		return Objects.hash(email, fname, id, lname, password, status, username);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -67,18 +102,11 @@ public class User {
 		User other = (User) obj;
 		return Objects.equals(email, other.email) && Objects.equals(fname, other.fname) && id == other.id
 				&& Objects.equals(lname, other.lname) && Objects.equals(password, other.password)
-				&& Objects.equals(username, other.username);
-	}
-	
-	//To String
-	
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + ", fname=" + fname + ", lname="
-				+ lname + ", email=" + email + "]";
+				&& status == other.status && Objects.equals(username, other.username);
 	}
 	
 	
+
 	
 	
 	
