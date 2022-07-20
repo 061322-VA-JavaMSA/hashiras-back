632adcae603ac64eb4d198684b62bb57345654e3
Glenn Syham <glennsyham@gmail.com>
2022-07-20 13:07:45 -0700

    fixed fields on aimelist to match table

--- src/main/java/com/revature/models/AnimeList.java
+++ src/main/java/com/revature/models/AnimeList.java
@@ -2,6 +2,7 @@ package com.revature.models;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -12,36 +13,48 @@ import javax.persistence.Table;
 @Entity
 @Table(name="lists")
 public class AnimeList {
-	private int animeID;
+	@Column
+	private int anime_id;
 	
     @ManyToOne
     @JoinColumn(name = "id")	
 	private User user;
     
-	private int rating;
+    @Column
+	private String usercomment;
 	
+    @Column
+	private int user_rating;
+    
 	@Enumerated(EnumType.STRING)
 	private ListStatus status;
 	
 	//Getters and Setters
 	
-	public int getAnimeID() {
-		return animeID;
-	}
-	public void setAnimeID(int animeID) {
-		this.animeID = animeID;
-	}
+ 
 	public User getUser() {
 		return user;
 	}
 	public void setUser(User user) {
 		this.user = user;
 	}
-	public int getRating() {
-		return rating;
+	public int getAnime_id() {
+		return anime_id;
 	}
-	public void setRating(int rating) {
-		this.rating = rating;
+	public void setAnime_id(int anime_id) {
+		this.anime_id = anime_id;
+	}
+	public String getUsercomment() {
+		return usercomment;
+	}
+	public void setUsercomment(String usercomment) {
+		this.usercomment = usercomment;
+	}
+	public int getUser_rating() {
+		return user_rating;
+	}
+	public void setUser_rating(int user_rating) {
+		this.user_rating = user_rating;
 	}
 	public ListStatus getStatus() {
 		return status;
@@ -49,12 +62,9 @@ public class AnimeList {
 	public void setStatus(ListStatus status) {
 		this.status = status;
 	}
-	
-	//Getters and Setters
-
 	@Override
 	public int hashCode() {
-		return Objects.hash(animeID, rating, status, user);
+		return Objects.hash(anime_id, status, user, user_rating, usercomment);
 	}
 	@Override
 	public boolean equals(Object obj) {
@@ -65,16 +75,18 @@ public class AnimeList {
 		if (getClass() != obj.getClass())
 			return false;
 		AnimeList other = (AnimeList) obj;
-		return animeID == other.animeID && rating == other.rating && status == other.status
-				&& Objects.equals(user, other.user);
+		return anime_id == other.anime_id && status == other.status && Objects.equals(user, other.user)
+				&& user_rating == other.user_rating && Objects.equals(usercomment, other.usercomment);
 	}
-	
-	//To String
-	
 	@Override
 	public String toString() {
-		return "List [animeID=" + animeID + ", user=" + user + ", rating=" + rating + ", status=" + status + "]";
+		return "AnimeList [anime_id=" + anime_id + ", user=" + user + ", usercomment=" + usercomment + ", user_rating="
+				+ user_rating + ", status=" + status + "]";
 	}
+ 
+	//Getters and Setters
+
+	 
 	
 
 	
