48b003f12b662f6aa1e00d4d540dfd2bf4cee48f
Glenn Syham <glennsyham@gmail.com>
2022-07-20 12:53:09 -0700

    added animelist services and repository

--- src/main/java/com/revature/repositories/AnimeListRepository.java
+++ src/main/java/com/revature/repositories/AnimeListRepository.java
@@ -1,11 +1,16 @@
 package com.revature.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.models.AnimeList;
 import com.revature.models.User;
 
+@Repository
 public interface AnimeListRepository extends JpaRepository<AnimeList, Integer> {
- 	User findAnimeListByUser(User user);
-
+	List<AnimeList>  findAnimeListByUser(User user);
+ 	List<AnimeList> findAnimeListByUserAnimeId(User user,int anime_id);
+ 	List<AnimeList> findAnimeListByStatus(User user,int anime_id,String status);
 }
