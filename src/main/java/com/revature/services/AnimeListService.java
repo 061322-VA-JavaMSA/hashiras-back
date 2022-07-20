429a8635dc5fafce6ca211310074f11cb0c07ac5
Glenn Syham <glennsyham@gmail.com>
2022-07-20 12:49:03 -0700

    added animelist services and repository

---
+++ src/main/java/com/revature/services/AnimeListService.java
@@ -0,0 +1,62 @@
+package com.revature.services;
+
+import java.util.List;
+
+import javax.persistence.Id;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.revature.exceptions.AnimeListNotFoundException;
+import com.revature.models.AnimeList;
+import com.revature.models.User;
+import com.revature.repositories.AnimeListRepository;
+
+@Service
+public class AnimeListService {
+	/**
+	 * 
+	 */
+	public AnimeListService() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private AnimeListRepository alr;
+	
+	public List<AnimeList> getAnimeList(){
+		return alr.findAll();
+	}
+ 
+	
+	@Transactional
+	public AnimeList getAnimeListById(int id) throws AnimeListNotFoundException {
+		AnimeList animelist = alr.findById(id).orElseThrow(() -> new AnimeListNotFoundException());
+		return animelist;
+	}
+	
+	@Transactional
+	public List<AnimeList> findAnimeListByUser(User user) {
+		List<AnimeList> animelist = alr.findAnimeListByUser(user);
+		return animelist;
+	}
+
+	@Transactional
+	public List<AnimeList> findAnimeListByUserAnimeId(User user,int anime_id) {
+		List<AnimeList> animelist = alr.findAnimeListByUserAnimeId(user,anime_id);
+		return animelist;
+	}
+	
+	@Transactional
+	public List<AnimeList> findAnimeListByStatus(User user,int anime_id,String status) {
+		List<AnimeList> animelist = alr.findAnimeListByStatus(user,anime_id,status);
+		return animelist;
+	}
+	
+	@Transactional
+	public AnimeList addAnimeList(AnimeList animelist) {
+ 		
+		return alr.save(animelist);
+	}
+	
+}
