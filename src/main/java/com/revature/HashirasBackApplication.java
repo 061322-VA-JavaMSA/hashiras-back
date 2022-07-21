10197fe32ca5cdd661eb5ee69c9679a5c2eecdbe
Glenn Syham <glennsyham@gmail.com>
2022-07-21 12:01:01 -0700

    add comment

--- src/main/java/com/revature/HashirasBackApplication.java
+++ src/main/java/com/revature/HashirasBackApplication.java
@@ -33,7 +33,7 @@ public class HashirasBackApplication {
 }
 //@Component
 //class AppStartupRunner implements ApplicationRunner {
-
+//
 //	@Autowired
 //	private AnimeListService as;
 //	@Autowired
@@ -44,7 +44,7 @@ public class HashirasBackApplication {
 //    public void run(ApplicationArguments args) throws Exception {
 //    	us.updateInfo("charles", "galbert","galbert@galbert.com", 1);
 //    	us.updatePassword("12345", 1);
-
+//
 //		List<AnimeList> al = as.getAnimeList();
 //		System.out.println(al.toString());
 //    	User user = new User();
@@ -55,7 +55,7 @@ public class HashirasBackApplication {
 //    	animeList.setUser(user);
 //    	animeList.setStatus(ListStatus.CURRENTLY);
 //    	as.addAnimeList(animeList);
-    	
+//    	
 //    	List<User> users = us.getUsers();
 //    	System.out.println(users.toString());
 //    	user.setUsername("charles");;
@@ -68,6 +68,6 @@ public class HashirasBackApplication {
 //    	User principal = ur.findUserByUsername("LaneM123");
 //    	
 //    	System.out.println(principal);
- 
+    	
 //    }
 //}
--- src/main/java/com/revature/repositories/AnimeListRepository.java
+++ src/main/java/com/revature/repositories/AnimeListRepository.java
@@ -2,7 +2,11 @@ package com.revature.repositories;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.models.AnimeList;
@@ -15,5 +19,7 @@ public interface AnimeListRepository extends JpaRepository<AnimeList, Integer> {
 	AnimeList findAnimeListByUserAndAnimeId(User user,int anime_id);
 	List<AnimeList>  findAnimeListByAnimeId(int anime_id);
 	List<AnimeList> findAnimeListByUserAndAnimeIdAndStatus(User user,int anime_id,String status);
-
+	@Transactional 
+	@Modifying
+	@Query("update AnimeList set status = ?1 where animeId = ?2 and user = ?3") int updateStatus(ListStatus status,int anime_id,User user);
 }
