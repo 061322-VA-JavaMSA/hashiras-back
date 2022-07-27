1c97d19c7df9336927e1a1a1eff325ff387383ea
Glenn Syham <glennsyham@gmail.com>
2022-07-26 23:21:00 -0700

    final push

--- src/main/java/com/revature/controllers/AnimeListController.java
+++ src/main/java/com/revature/controllers/AnimeListController.java
@@ -1,8 +1,8 @@
 package com.revature.controllers;
 
-import java.io.Console;
-import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,16 +11,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.dto.AnimeListDTO;
 import com.revature.dto.ReqAnimeListDTO;
+import com.revature.exceptions.AnimeListNotFoundException;
 import com.revature.exceptions.UserNotFoundException;
 import com.revature.models.AnimeList;
 import com.revature.models.ListStatus;
 import com.revature.models.User;
+import com.revature.repositories.AnimeListRepository;
 import com.revature.services.AnimeListService;
 import com.revature.services.UserService;
 
@@ -28,25 +32,29 @@ import com.revature.services.UserService;
 @RestController
 @RequestMapping("/anime")
 public class AnimeListController {
-	@Autowired
+ 
 	private AnimeListService as;
 	
-	@Autowired
+	
 	private UserService us;
 	
+	private AnimeListRepository alr ;
+	@Autowired
+	public AnimeListController(UserService us,AnimeListService as) {
+		super();
+		// TODO Auto-generated constructor stub
+		this.us = us;
+		this.as = as;
+	}
+	
 	@GetMapping("/{id}")
-	public ResponseEntity<List<AnimeListDTO>> getUserById(@PathVariable("id") int id){
-			List<AnimeListDTO> animeDTO = new ArrayList<>();
-			List<AnimeList> anime = null;
-			
+	public ResponseEntity<AnimeListDTO> getById(@PathVariable("id") int id){
+			AnimeListDTO animeDTO = null;
+ 			
 			try {
-				anime = as.findAnimeListByUser(us.getUserById(id));
-			} catch (UserNotFoundException e) {
-				e.printStackTrace();
-			}
-			
-			for(AnimeList al : anime) {
-				animeDTO.add(new AnimeListDTO(al));
+				animeDTO = new AnimeListDTO(as.getAnimeListById(id));
+			} catch (AnimeListNotFoundException e) {
+				return new ResponseEntity<>(animeDTO, HttpStatus.NOT_ACCEPTABLE);
 			}
 			
 			return new ResponseEntity<>(animeDTO, HttpStatus.OK);
@@ -54,36 +62,48 @@ public class AnimeListController {
 	
 	/* Get Anime List by User ID and Anime ID */
 	@GetMapping("/user/{user_id}/anime/{anime_id}")
-	public ResponseEntity<AnimeList> findAnimeListByUserAndAnimeId(@PathVariable("user_id") int user_id,@PathVariable("anime_id") int anime_id){
+	public ResponseEntity<AnimeListDTO> findAnimeListByUserAndAnimeId(@PathVariable("user_id") int user_id,@PathVariable("anime_id") int anime_id){
  			AnimeList anime = null;
 			User user = new User();
- 			
+			AnimeListDTO animeDTO = null;
 			try {
 				user = us.getUserById(user_id);
-				anime = as.findAnimeListByUserAndAnimeId(user,anime_id);
+				
 			} catch (UserNotFoundException e) {
-				e.printStackTrace();
+				return new ResponseEntity<>(animeDTO, HttpStatus.NOT_ACCEPTABLE);
 			}
-			
- 
-			return new ResponseEntity<>(anime, HttpStatus.OK);
+			if(user != null) {
+				anime = as.findAnimeListByUserAndAnimeId(user,anime_id);
+			}
+			if(anime != null) {
+				animeDTO = new AnimeListDTO(anime);
+
+			} else {
+				return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+			}
+			 
+			return new ResponseEntity<>(animeDTO, HttpStatus.OK);
 	}
 	
 	/* Get anime by user */
 	@GetMapping("/user/{user_id}")
-	public ResponseEntity<List<AnimeList>> findAnimeListByUser(@PathVariable("user_id") int user_id){
+	public ResponseEntity<List<AnimeListDTO>> findAnimeListByUser(@PathVariable("user_id") int user_id){
 			List<AnimeList> anime = null;
 			User user = new User();
- 			
+			List<AnimeListDTO> animeListDTO = null;
+
 			try {
 				user = us.getUserById(user_id);
 				anime = as.findAnimeListByUser(user);
 			} catch (UserNotFoundException e) {
-				e.printStackTrace();
+				return new ResponseEntity<>(animeListDTO, HttpStatus.NOT_ACCEPTABLE);
 			}
-			
- 
-			return new ResponseEntity<>(anime, HttpStatus.OK);
+ 			for(AnimeList al : anime) {
+ 				animeListDTO.add(new AnimeListDTO(al));
+			}
+			 
+			 
+			return new ResponseEntity<>(animeListDTO, HttpStatus.OK);
 	}
 	
 	/* Add an anime to the user's list */
@@ -92,8 +112,9 @@ public class AnimeListController {
  		User u = new User();
  		AnimeList al = new AnimeList();
  		al.setStatus(ListStatus.DROPPED);
+ 		al.setUser(u);
  		AnimeListDTO animeListDTO = new AnimeListDTO(al);
- 		try {
+  		try {
 			u = us.getUserById(reqAnimeListDTO.getUser_id());
 		} catch (UserNotFoundException e) {
 			return new ResponseEntity<>(animeListDTO, HttpStatus.NOT_ACCEPTABLE);
@@ -111,7 +132,28 @@ public class AnimeListController {
 					animeListDTO = new AnimeListDTO(newAnimeList);
 					return new ResponseEntity<>(animeListDTO, HttpStatus.CREATED);
  				}
-		}
-		return new ResponseEntity<>(animeListDTO, HttpStatus.NOT_ACCEPTABLE);	
+		} 
+		return new ResponseEntity<>(animeListDTO, HttpStatus.CONFLICT);	
  	}
+	
+	@PutMapping("/{id}/status")
+	public ResponseEntity<Boolean> updateStatusById(@PathVariable("id") int id,@RequestParam(name="status", required=false) String reqStatus){	
+	 
+		ListStatus status = ListStatus.valueOf(reqStatus)  ;
+		int num = as.updateStatusById(status,id);
+		boolean bol = (num >= 1) ? true:false;
+		return new ResponseEntity<>(bol, HttpStatus.ACCEPTED);	
+	}
+	
+	
+	@PutMapping("/{id}/rate")
+	public ResponseEntity<Boolean> updateRatingById(@PathVariable("id") int id,@RequestParam(name="user_rating", required=false) int user_rating){
+
+ 		if(user_rating <= 0 || user_rating > 10) {
+			return new ResponseEntity<>(false, HttpStatus.NOT_ACCEPTABLE);
+		}
+ 		int num = as.updateRatingById(user_rating,id);
+		boolean bol = (num >= 1) ? true:false;
+		return new ResponseEntity<>(bol, HttpStatus.ACCEPTED);	
+	}
 }
